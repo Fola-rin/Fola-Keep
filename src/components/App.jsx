0406b4c096edf652d8a8b649469e266f53b9331b
Oyeleke Afolarin <oyelekeafolarin@Oyelekes-MacBook-Pro.local>
2021-02-09 18:28:33 +0100

    Homme page edited

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -57,7 +57,7 @@ function App() {
 					className="switch-wrapper"
 				>
 					<div>
-						<Route path="/" component={Home} exact />
+						<Route path="/Fola-Keep" component={Home} exact />
 						<Route path="/about" component={About} />
 					</div>
 				</AnimatedSwitch>
